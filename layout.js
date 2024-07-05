e9b0871c3c8b593af3d9ffb091209d4d4798ad25
Alan <115372720+4lankurd@users.noreply.github.com>
2024-07-05 16:26:52 +0300

    Add files via upload

--- layout.js
+++ layout.js
@@ -3,6 +3,7 @@ var bodyTheme = document.querySelector(".bodyTheme");
 var mainTheme = document.querySelector("main");
 var footerTheme = document.querySelector("footer");
 var navTheme = document.querySelector(".navbar");
+var cardTheme = document.querySelectorAll(".card");
 
 var previousTheme = "dark";
 var currentTheme = "dark";
@@ -13,6 +14,7 @@ function darkMode() {
     bodyTheme.style.color = "#f7e4e4d9";
     footerTheme.style.backgroundColor = "#101e23";
     navTheme.style.backgroundColor = "#3d737f";
+    applyCardColor("#101e23");
 }
 
 function lightMode() {
@@ -21,6 +23,18 @@ function lightMode() {
     bodyTheme.style.color = "#071600";
     footerTheme.style.backgroundColor = "#101e23";
     navTheme.style.backgroundColor = "#101e23";
+    applyCardColor("#101e232c");
+
+}
+function applyCardColor(color) {
+    for (var i = 0; i <cardTheme.length; i++) {
+         cardTheme[i].style.backgroundColor= color;
+    }
+
+
+
+
+
 }
 
 function choosingTheme() {
@@ -30,14 +44,13 @@ function choosingTheme() {
         currentTheme = "light";
         lightMode();
         darkbtn.textContent = "Dark";
-        console.log(currentTheme);
     } else {
         // Save current theme as previous theme before changing
         previousTheme = currentTheme;
         currentTheme = "dark";
         darkMode();
         darkbtn.textContent = "Light";
-        console.log(currentTheme);
+        
     }
 }
 
